ea07a98ce4a0376eca33403dd651ce267f1572e8
Anuj Dubey <anujd2408@gmail.com>
2025-01-18 01:57:47 +0530

    Adding

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,15 @@
 import type { Config } from "tailwindcss";
+import fluid, {extract} from "fluid-tailwind";
 
 export default {
-  content: [
+  content: {files:[
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/slices/**/*.{js,ts,jsx,tsx,mdx}"
   ],
+  extract
+},
   theme: {
     extend: {
       colors: {
@@ -15,5 +18,5 @@ export default {
       },
     },
   },
-  plugins: [],
+  plugins: [fluid],
 } satisfies Config;
