fe114b013a6c390d5f76d3e8671304c79a75b96b
Anuj Dubey <anujd2408@gmail.com>
2025-01-21 20:35:16 +0530

    Adding

--- src/slices/ProductGrid/index.tsx
+++ src/slices/ProductGrid/index.tsx
@@ -1,4 +1,5 @@
 import { Bounded } from "@/components/Bounded";
+import { Heading } from "@/components/Heading";
 import { Content } from "@prismicio/client";
 import { PrismicNextLink } from "@prismicio/next";
 import { PrismicRichText, SliceComponentProps } from "@prismicio/react";
@@ -17,8 +18,12 @@ const ProductGrid = ({ slice }: ProductGridProps): JSX.Element => {
     <Bounded
       data-slice-type = {slice.slice_type}
       data-slice-variation = {slice.variation}
+      className="bg-texture bg-Dark-Charcoal"
       >
-        <PrismicRichText field={slice.primary.heading} />
+        <Heading>
+          <PrismicRichText field={slice.primary.heading} />
+        </Heading>
+        
         <PrismicRichText field={slice.primary.body} />
 
         {slice.primary.product.map((item,index) => (
