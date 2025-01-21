c9d04eeffc704dd5036f3d217f495ff670163388
Anuj Dubey <anujd2408@gmail.com>
2025-01-21 20:48:13 +0530

    Adding

--- src/slices/ProductGrid/ConsoleProduct.tsx
+++ src/slices/ProductGrid/ConsoleProduct.tsx
@@ -1,5 +1,6 @@
 import { createClient } from '@/prismicio';
 import { Content } from '@prismicio/client';
+import { PrismicNextImage } from '@prismicio/next';
 import React from 'react'
 
 type Props = {
@@ -11,7 +12,9 @@ async function ConsoleProduct({id}: Props) {
     const product = await client.getByID<Content.
     ConsoleDocument>(id);
   return (
-    <div>ConsoleProduct</div>
+    <div>
+        <PrismicNextImage alt='' field={product.data.image} width={150}/>
+    </div>
   )
 }
 
