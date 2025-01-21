3e088b88c29027a6f96e8ffc0776d3b1c6066198
Anuj Dubey <anujd2408@gmail.com>
2025-01-21 20:46:54 +0530

    Adding

--- src/slices/ProductGrid/ConsoleProduct.tsx
+++ src/slices/ProductGrid/ConsoleProduct.tsx
@@ -1,12 +1,15 @@
 import { createClient } from '@/prismicio';
+import { Content } from '@prismicio/client';
 import React from 'react'
 
 type Props = {
     id:string;
 }
 
-function ConsoleProduct({}: Props) {
+async function ConsoleProduct({id}: Props) {
     const client = createClient();
+    const product = await client.getByID<Content.
+    ConsoleDocument>(id);
   return (
     <div>ConsoleProduct</div>
   )
