69bd48feb6b4f008b86fc6b5ba6b487a011e15db
Anuj Dubey <anujd2408@gmail.com>
2025-01-22 21:04:20 +0530

    Adding

--- src/slices/TextAndImage/index.tsx
+++ src/slices/TextAndImage/index.tsx
@@ -2,6 +2,7 @@ import { Bounded } from "@/components/Bounded";
 import { Content } from "@prismicio/client";
 import { PrismicNextImage, PrismicNextLink } from "@prismicio/next";
 import { PrismicRichText, SliceComponentProps } from "@prismicio/react";
+import clsx from "clsx";
 import { JSX } from "react";
 
 /**
@@ -13,10 +14,17 @@ export type TextAndImageProps = SliceComponentProps<Content.TextAndImageSlice>;
  * Component for "TextAndImage" Slices.
  */
 const TextAndImage = ({ slice }: TextAndImageProps): JSX.Element => {
+  const theme = slice.primary.theme;
   return (
     <Bounded
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
+      className={clsx(
+        theme === "Blue" && "bg-blue-500 text-white",
+        theme === "Green" && "bg-green-500 text-white",
+        theme === "Orange" && "bg-orange-500 text-white",
+        theme === "Purple" && "bg-purple-500 text-white"
+      )}
     >
      {slice.primary.theme}
      <PrismicRichText field={slice.primary.heading} />
