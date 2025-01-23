572cc8b20d80160422c4f875783f5cdb7122d4e2
Anuj Dubey <anujd2408@gmail.com>
2025-01-23 20:03:52 +0530

    Adding

--- src/slices/TextAndImage/ParallaxImage.tsx
+++ src/slices/TextAndImage/ParallaxImage.tsx
@@ -1,22 +1,29 @@
 import React from 'react'
 import { ImageField } from '@prismicio/client'
 import { PrismicNextImage } from '@prismicio/next';
+import clsx from 'clsx';
 
 type Props = {
     foregroundImage: ImageField;
     backgroundImage : ImageField;
-    classname?: string;
+    className?: string;
 }
 
 export default function ParallaxImage({
     foregroundImage,
     backgroundImage,
-    classname,
+    className,
 }: Props) {
   return (
-    <div>
-        <PrismicNextImage field={backgroundImage} alt=''/>
-        <PrismicNextImage field={foregroundImage} alt=''/>
+    <div className={clsx("grid grid-cols-1 place-items-center", className)}>
+        <div className='col-start-1 row-start-1 transition-transform'>
+            <PrismicNextImage field={backgroundImage} alt=''/>
+        </div>
+        
+        <div className='col-start-1 row-start-1 transition-transform'>
+            <PrismicNextImage field={foregroundImage} alt=''/>
+        </div>
+        
     </div>
   );
 }
