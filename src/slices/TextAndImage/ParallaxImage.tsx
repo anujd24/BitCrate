d95fe1a29c083023b803606e9ed54079e11f6bbf
Anuj Dubey <anujd2408@gmail.com>
2025-01-23 20:22:02 +0530

    Adding

--- src/slices/TextAndImage/ParallaxImage.tsx
+++ src/slices/TextAndImage/ParallaxImage.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import { ImageField } from '@prismicio/client'
 import { PrismicNextImage } from '@prismicio/next';
 import clsx from 'clsx';
@@ -16,6 +16,21 @@ export default function ParallaxImage({
     backgroundImage,
     className,
 }: Props) {
+
+    const backgroundRef = useRef<HTMLDivElement>(null);
+    const foregroundRef = useRef<HTMLDivElement>(null);
+
+    const targetPosition = useRef({ x:0, y:0 });
+
+    useEffect(() => {
+        window.addEventListener("mousemove", onMouseMove);
+
+        function onMouseMove(event:MouseEvent){
+            const { innerWidth, innerHeight } = window;
+        }
+
+    })
+
   return (
     <div className={clsx("grid grid-cols-1 place-items-center", className)}>
         <div className='col-start-1 row-start-1 transition-transform'>
