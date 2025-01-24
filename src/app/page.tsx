c1b18f53abcc2fdebf116eba15e6fddc16e5dc8c
Anuj Dubey <anujd2408@gmail.com>
2025-01-24 21:03:31 +0530

    Adding

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,38 @@
 import { Metadata } from "next";
-import { isFilled, asImageSrc, Content } from "@prismicio/client";
-import { SliceZone } from "@prismicio/react";
+import { Content } from "@prismicio/client";
+import { SliceComponentProps, SliceZone } from "@prismicio/react";
 
 import { createClient } from "@/prismicio";
 import { components } from "@/slices";
 
+
+
 export default async function Page() {
   const client = createClient();
   const page = await client.getSingle("homepage");
+  const slices = bundleTextAndImageSlices(page.data.slices);
 
-
-  return <SliceZone slices={page.data.slices} components={components} />;
+  return (
+    <SliceZone
+      slices={slices}
+      components={{
+        ...components,
+        text_and_image_bundle: ({
+          slice,
+        }: SliceComponentProps<TextAndImageBundleSlice>) => (
+          <div>
+            <SliceZone slices={slice.slices} components={components} />
+          </div>
+        ),
+      }}
+    />
+  );
 }
 
 export async function generateMetadata(): Promise<Metadata> {
   const client = createClient();
   const page = await client.getSingle("homepage");
+  const slices = bundleTextAndImageSlices(page.data.slices);
 
   return {
     title: page.data.meta_title,
