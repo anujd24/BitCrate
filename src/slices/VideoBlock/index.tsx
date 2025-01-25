da27b39c9de8fb63d512659d9e15b8e24dcb47cb
Anuj Dubey <anujd2408@gmail.com>
2025-01-25 19:59:27 +0530

    Adding

--- src/slices/VideoBlock/index.tsx
+++ src/slices/VideoBlock/index.tsx
@@ -2,6 +2,11 @@ import { Bounded } from "@/components/Bounded";
 import { Content } from "@prismicio/client";
 import { SliceComponentProps } from "@prismicio/react";
 import { JSX } from "react";
+import { LazyYouTubePlayer } from "./LazyYoutubePlayer";
+import clsx from "clsx";
+
+const MASK_CLASSES =
+  "[mask-image:url(/video-mask.png)] [mask-mode:alpha] [mask-position:center_center] [mask-repeat:no-repeat] [mask-size:100%_auto]";
 
 /**
  * Props for `VideoBlock`.
@@ -20,7 +25,13 @@ const VideoBlock = ({ slice }: VideoBlockProps): JSX.Element => {
     >
       <h2 className="sr-only">Video</h2>
       <div className="relative aspect-video">
-
+      <div
+          className={clsx(
+            MASK_CLASSES,
+            "bg-brand-lime absolute inset-0 ~translate-x-2/3 ~translate-y-2/3"
+          )}
+        />
+        <LazyYouTubePlayer youTubeID={slice.primary.youtube_video_id}/>
       </div>
     </Bounded>
   );
