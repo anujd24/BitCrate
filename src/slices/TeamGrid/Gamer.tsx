62f6e6a29a1d09a919892a3a9a4e8b6a2c15127b
Anuj Dubey <anujd2408@gmail.com>
2025-01-27 23:21:56 +0530

    Adding

--- src/slices/TeamGrid/Gamer.tsx
+++ src/slices/TeamGrid/Gamer.tsx
@@ -13,7 +13,7 @@ export default function Gamer({gamer, index}: Props) {
         <div className='stack-layout overflow-hidden'>
             <PrismicNextImage field = {gamer.data.photo_background} width={500} 
             imgixParams={{q:20}} alt="" className="scale-110 transform transition-all
-            duration-100 ease-in-out group-hover:scale-10 group-hover:brightness-75
+            duration-100 ease-in-out group-hover:scale-100 group-hover:brightness-75
             group-hover:saturate-[.8]"/>
             <PrismicNextImage field = {gamer.data.photo_foreground} width={500} 
             imgixParams={{q:20}} alt="" className="transform transition-transform
