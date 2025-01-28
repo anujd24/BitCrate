4ceffd0ca7400a6ffab4c5140d28578da476c53b
Anuj Dubey <anujd2408@gmail.com>
2025-01-28 21:29:58 +0530

    Adding

--- src/slices/Hero/InteractiveConsole.tsx
+++ src/slices/Hero/InteractiveConsole.tsx
@@ -2,14 +2,18 @@
 
 import React, { Suspense } from 'react'
 import { Canvas } from "@react-three/fiber"
+import { Environment, OrbitControls } from "@react-three/drei"
 import { BoxGeometry, Group, Mesh, MeshBasicMaterial } from 'three'
 
 type Props = {}
 
 export default function InteractiveConsole({}: Props) {
   return (
-    <div>
-        <Canvas> 
+    <div className='absolute inset-0 z-10 flex items-center justify-center'>
+        <Canvas 
+        className='min-h-[60rem] w-full'
+        camera={{position: [1.5, 1, 1.4], fov: 55}}
+        > 
             <Suspense>
                 <Scene/>
             </Suspense>
@@ -21,8 +25,10 @@ export default function InteractiveConsole({}: Props) {
 function Scene (){
     return (
         <group>
+            <OrbitControls/>
+            <Environment preset='forest' background />
             <mesh>
-                <meshBasicMaterial/>
+                <meshStandardMaterial/>
                 <boxGeometry/>
             </mesh>
         </group>
