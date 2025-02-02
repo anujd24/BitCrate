0fb7b4d540260782329b959c8923140e5ebc4dc2
Anuj Dubey <anujd2408@gmail.com>
2025-02-02 15:29:07 +0530

    Adding

--- src/components/Bounded.tsx
+++ src/components/Bounded.tsx
@@ -3,22 +3,22 @@ import React, { CSSProperties, ElementType, ReactNode } from "react";
 
 type BoundedProps = {
   as?: ElementType;
-  className?: string | undefined;
+  className?: string;
   style?: CSSProperties;
-  children?: ReactNode; // 👈 Fix TypeScript issue
+  children?: ReactNode; // 👈 Explicitly define the type of children
 };
 
 export function Bounded({
-  as: Comp = "section",
+  as: Comp = "section", // 👈 Default to "section" if `as` is not provided
   className,
-  children = null, // 👈 Ensure children is not `never`
+  children,
   ...restProps
 }: BoundedProps) {
   return (
     <Comp
       className={clsx(
         "px-6 ~py-10/16 [.header+&]:pt-44 [.header+&]:md:pt-32",
-        className as string
+        className
       )}
       {...restProps}
     >
